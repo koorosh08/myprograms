5f03606f49f489f71bdb26aecc1793a827580f12
Koorosh08 <kooroshvafadoost2121@gmail.com>
2025-09-15 10:52:11 -0400

    created a python file to be creepy

---
+++ p1.py
@@ -0,0 +1,7 @@
+# my first program
+# created on 09/15/2025
+# Editor: Koorosh
+
+name = input("hey there, can i have your name?")
+age = input( "hey " + name +" how old are you?")
+print(name + " is "+ age + " yearsq old")
